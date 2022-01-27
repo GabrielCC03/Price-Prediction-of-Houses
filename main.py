297e11418e935b1ec73b373d513296d77f259cd4
GabrielCC03 <71728431+GabrielCC03@users.noreply.github.com>
2022-01-27 14:06:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import pandas as pd
 
 import sklearn as sk
 
-from matplotlib import plt
+import matplotlib
 
-import Seaborn
+import seaborn
+
+from sklearn.datasets import load_boston
+
+data = load_boston()
+
+url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(url, sep="s+", skiprows=22, header=None)
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+
+print(raw_df)
