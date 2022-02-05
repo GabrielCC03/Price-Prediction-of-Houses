ee6fad7c7b8861853508c1f74e07ad91089266e5
GabrielCC03 <71728431+GabrielCC03@users.noreply.github.com>
2022-02-05 15:00:08 +0800

    Update

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ target = data.values[: , 2] #[1::2, 2]
 data["MEDV"] = target
 
 #!_----------------------------------------------------------------
+#Plots
+"""
 seaborn.boxplot(data=data)
 #plt.show()
 
@@ -44,6 +46,8 @@ seaborn.kdeplot(data=data["MEDV"])
 seaborn.catplot(kind="bar", data=data)
 #plt.show()
 
+"""
+
 #!-------------------------------------------
 
 x = data[['age','tax','rm','lstat','dis','zn','indus']]
@@ -51,40 +55,72 @@ y = data.MEDV
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3,random_state = 5 )
 
-#Linear regression Prediction
+#* Linear regression Prediction
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
-y_pred = regressor.predict(x_test)
-predictLR = pd.DataFrame({'Actual price': y_test, 'Predicted price': y_pred})
-predictLR.sort_index()
+linearRegressionPredict = regressor.predict(x_test)
+linearRegressionDF = pd.DataFrame({'Actual price': y_test, 'Predicted price': linearRegressionPredict})
+linearRegressionDF.sort_index()
 
 #* Normalized Data feature selection
-xN = data[['age','tax','rm','lstat','dis','zn','indus']]
-yN = data.MEDV
 
-xN_train, xN_test, yN_train, yN_test = train_test_split(xN,yN, test_size = 0.3,random_state = 5 )
+xN_train, xN_test, yN_train, yN_test = train_test_split(x,y, test_size = 0.3,random_state = 5 )
 
 #RandomForestRegressor
-rfr = RandomForestRegressor(max_depth=2, random_state=0)
-rfr.fit(xN_train, yN_train)
-rfrPredict = rfr.predict(xN_test)
-predictRfr = pd.DataFrame({'Actual price': yN_test, 'Predicted price': rfrPredict})
-predictRfr.sort_index()
+randomForest = RandomForestRegressor(max_depth=2, random_state=1 )
+randomForest.fit(xN_train, yN_train)
+randomForestPredict = randomForest.predict(xN_test)
+randomForestDF = pd.DataFrame({'Actual price': yN_test, 'Predicted price': randomForestPredict})
+randomForestDF.sort_index()
 
-#Support Vector Regressor  
+#* Support Vector Regressor  
 svm = svm.SVR()
 svm.fit(xN_train, yN_train)
 svmPredict = svm.predict(xN_test)
-predictSVM = pd.DataFrame({'Actual price': yN_test, 'Predicted price': svmPredict})
+svmDF = pd.DataFrame({'Actual price': yN_test, 'Predicted price': svmPredict})
+svmDF.sort_index()
 
 #print(data)
 print("Linear Regression: ")
-print(predictLR)
+print(linearRegressionDF)
 print("Random Forest Regressor: ")
-print(predictRfr)
+print(randomForestDF)
 print("Support Vector Machine Regressor: ")
-print(predictSVM)
+print(svmDF)
 
+#* Analyzing data
+#? Mean Absolute error
+print("Mean sAbsolute Error: ")
+print("Linear Regression:",metrics.mean_absolute_error(y_test,linearRegressionPredict))
+print("Random Forest:",metrics.mean_absolute_error(yN_test,randomForestPredict))
+print("Support Vector Machine:",metrics.mean_absolute_error(yN_test,svmPredict))
+
+#? Mean squared error
+print("\nMean Squared Error: ")
+print("Linear Regression:",metrics.mean_squared_error(y_test,linearRegressionPredict))
+print("Random Forest:",metrics.mean_squared_error(yN_test,randomForestPredict))
+print("Support Vector Machine:",metrics.mean_squared_error(yN_test,svmPredict))
+
+#? R2 Score
+print("\nR2 Score: ")
+print("Linear Regression:",metrics.r2_score(y_test,linearRegressionPredict))
+print("Random Forest:",metrics.r2_score(yN_test,randomForestPredict))
+print("Support Vector Machine:",metrics.r2_score(yN_test,svmPredict))
+
+#? Plots
+x1 = np.array(x_test["tax"])
+y1 = np.array(linearRegressionPredict)
+m,b= np.polyfit(x1,y1,1)
+
+#* Plots of Results:
+plt.plot(x_test["tax"], m*x_test["tax"] + b)
+plt.plot(x_test["tax"],linearRegressionPredict,'o')
+plt.xlabel("Tax")
+plt.ylabel("Median Value")
+plt.show()
+
+seaborn.pairplot(data,x_vars=['age','tax','rm','lstat','dis','zn','indus'],y_vars="MEDV",kind="reg")
+plt.show()
 
 
 #data.info()
